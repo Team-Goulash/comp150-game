89398b6ccc4e7b4a0478e9c6afd5cc3052828650
Ashley Sands <as223343@falmouth.ac.uk>
2018-11-09 13:48:35 +0000

    Change. text is now centered on buttons and labels

--- menuUi.py
+++ menuUi.py
@@ -1,21 +1,26 @@
 import pygame
 import UI
 
-class UiMenu:
 
-    pygame = None
-    active = False
+class UiMenu:
+    """ Creat a set of menus controlled by a state
+    Drive: Ashley.
+    """
+
+    active = False      # needed??
     current_menu = None
-    # default fontfasces. use set_font_face to change.
+    # default fontfaces. use set_font_face to change.
     header_fontface = None
     button_fontface = None
     menus = {}
     buttons = {}
 
+    HEADER_Y_POSITION = 75
+    HEADER_SIZE = (500, 100)
+
     def __init__(self):
         self.header_fontface = pygame.font.SysFont("arial", 100)
         self.button_fontface = pygame.font.SysFont("arial", 50)
-
 
     def set_font_face(self, header_fontface, button_fontface):
         """ set the fontface, params are ignored if None """
@@ -42,7 +47,8 @@ class UiMenu:
         """ Add a new menu to list of menus """
         self.menus[menu_title] = []
 
-    def add_button(self, menu_title, button_type, button_label, position_rect, action_funct):
+    def add_button(self, menu_title, button_type,
+                   button_label, position_rect, action_funct):
         """
         Add a button to a menu group
         :param menu_title:      name of the group to add a button to
@@ -52,31 +58,55 @@ class UiMenu:
         :param action_funct:    the function with the button action
         :return:                None
         """
-        self.menus[menu_title].append([button_type, button_label, position_rect, action_funct])
+        self.menus[menu_title].append(
+            [button_type, button_label, position_rect, action_funct]
+        )
 
     def set_current_menu(self, menu_name):
         """set the menu to be displayed"""
         self.current_menu = menu_name
 
+    def center_surface(self, hold_surface_size, inner_surface_size):
+        """get the position to center the inner surface inside the hold surface.
+
+        :param hold_surface_size: Size of the surface holding the inner surface
+        :param inner_surface_size: size of the inner surface
+        :return: x, y offset to center center inner surface
+        """
+
+        offset_x = (hold_surface_size[0] - inner_surface_size[0]) // 2
+        offset_y = (hold_surface_size[1] - inner_surface_size[1]) // 2
+
+        return offset_x, offset_y
+
     def get_button(self, button, label, fontface, font_color=(0, 0, 0)):
         """get the button to be displayed with a label prefixed """
-        # reset the button
-        # add text to button
 
         temp_surface = button.copy()
         text_surface = fontface.render(label, True, font_color)
-        # Todo Center text to button
-        temp_surface.blit(text_surface, (0, 0))
+
+        # draw the text to the center of the button.
+        offset_x, offset_y = self.center_surface(
+            temp_surface.get_size(), text_surface.get_size()
+        )
+        temp_surface.blit(text_surface, (offset_x, offset_y))
 
         return temp_surface
 
-    def get_header(self, label, fontface, label_size, font_color=(0, 0, 0, 255), background_color=(255, 255, 255, 255)):
+    def get_header(self, label, fontface, label_size,
+                   font_color=(0, 0, 0, 255),
+                   background_color=(255, 255, 255, 255)
+                   ):
         """gets the menu header"""
+
         temp_surface = pygame.Surface(label_size)
         temp_surface.fill(background_color)
-        # Todo center text on label
         text_surface = fontface.render(label, True, font_color)
-        temp_surface.blit(text_surface, (0, 0))
+
+        # draw the text to the center of the header label.
+        offset_x, offset_y = self.center_surface(temp_surface.get_size(),
+                                                 text_surface.get_size())
+        temp_surface.blit(text_surface, (offset_x, offset_y))
 
         return temp_surface
 
@@ -84,16 +114,31 @@ class UiMenu:
         """Draws all the buttons and menu header to the surface"""
         # note to self: button_type, button_label, position_rect, action_funct
 
-        # display the header onto the surface
-        # Todo center the label on the button
-        surface.blit(self.get_header(self.current_menu, self.header_fontface, (250, 50)), (250, 75))
+        # display the header in the hoz center of the screen
+        header_surface = self.get_header(self.current_menu,
+                                         self.header_fontface,
+                                         self.HEADER_SIZE)
+        offset_x, offset_y = self.center_surface(surface.get_size(),
+                                                 header_surface.get_size())
 
+        surface.blit(header_surface, (offset_x, self.HEADER_Y_POSITION))
+
+        # display all the buttons for the current menu.
         for button in self.menus[self.current_menu]:
-            surface.blit(self.get_button(self.buttons[button[0]].draw_button(mouse_pos, key_pressed, button[2]), button[1], self.button_fontface), button[2])
+            surface.blit(
+                self.get_button(
+                    self.buttons[button[0]].draw_button(
+                        mouse_pos, key_pressed, button[2]),
+                    button[1], self.button_fontface),
+                button[2]
+            )
 
     def is_button_pressed(self, mouse_position, mouse_is_pressed):
         """find if any of the buttons on the current menu are being pressed"""
-        # note to self. is buttons: cursor_pos, screen_pos, button_click
+
+        # See if any of the buttons are being pressed on the current menu.
         for button in self.menus[self.current_menu]:
-            if self.buttons[button[0]].is_pressed(mouse_position, button[2], mouse_is_pressed):
+            if self.buttons[button[0]].is_pressed(mouse_position,
+                                                  button[2], mouse_is_pressed
+                                                  ):
                 button[3]()
