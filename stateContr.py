23eb982723fb96e03ef597dcd4a304b772234868
ASHLEY SANDS <AS223343@falmouth.ac.uk>
2018-11-14 13:25:10 +0000

    Add doc strings to functions

--- stateContr.py
+++ stateContr.py
@@ -6,10 +6,13 @@ class StateController:
     states = {}
 
     def add_state(self, state_name, state_value):
+        """Adds a state to the list of states"""
         self.states[state_name] = state_value
 
     def set_state(self, state_name):
+        """Set the current state"""
         self.current_state = state_name
 
     def get_state(self):
+        """return the current state"""
         return self.states[self.current_state]
