934b16a55cf49f46b4c3e10868aa5c0b45eeb976
ASHLEY SANDS <AS223343@falmouth.ac.uk>
2018-11-14 13:02:39 +0000

    rename file time to timeManager

--- timeManager.py
+++ timeManager.py
@@ -1,14 +1,13 @@
 
-class Time:
+class TimeManager:
 
-    time = 0
+    last_time = 0
     delta_time = 0
 
     def __init__(self, current_time):
         self.time = current_time
-        pass
 
     def update_time(self, current_time):
         """Update the current time"""
-        self.delta_time = current_time - self.time
-        self.time = current_time
+        self.delta_time = current_time - self.last_time
+        self.last_time = current_time
